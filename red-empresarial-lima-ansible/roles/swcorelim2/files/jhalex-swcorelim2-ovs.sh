f6c6aa028609304c7ab5338de2257dbba39b521f
Katterine Alejandra Fernandez Blanco <ale123ferkatty@gmail.com>
2026-06-12 12:46:07 -0500

    fix(core): estabilizar bond core-core con active-backup

--- red-empresarial-lima-ansible/roles/swcorelim2/files/jhalex-swcorelim2-ovs.sh
+++ red-empresarial-lima-ansible/roles/swcorelim2/files/jhalex-swcorelim2-ovs.sh
@@ -17,7 +17,7 @@ ovs-vsctl --if-exists del-br br-core
 ovs-vsctl add-br br-core
 ovs-vsctl set Bridge br-core rstp_enable=true
 
-ovs-vsctl add-bond br-core bond-pcsc1-sc2 ens36 ens37 bond_mode=balance-slb lacp=off
+ovs-vsctl add-bond br-core bond-pcsc1-sc2 ens36 ens37 bond_mode=active-backup lacp=off other_config:bond-primary=ens36
 ovs-vsctl add-bond br-core bond-pcsc2-sd1 ens38 ens39 bond_mode=balance-slb lacp=off
 ovs-vsctl add-bond br-core bond-pcsc2-sd2 ens40 ens41 bond_mode=balance-slb lacp=off
 
