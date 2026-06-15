4079dc26db9e1b520492d499e7ec9d779cabe24f
Katterine Alejandra Fernandez Blanco <ale123ferkatty@gmail.com>
2026-06-15 07:25:36 -0500

    fix(core): consolidar port-channels core como trunks L2 sin IP

--- red-empresarial-lima-ansible/roles/swcorelim2/files/jhalex-swcorelim2-ovs.sh
+++ red-empresarial-lima-ansible/roles/swcorelim2/files/jhalex-swcorelim2-ovs.sh
@@ -15,23 +15,24 @@ ip link set ens41 up || true
 ovs-vsctl --if-exists del-br br-core
 
 ovs-vsctl add-br br-core
-ovs-vsctl set Bridge br-core rstp_enable=true
+ovs-vsctl set bridge br-core rstp_enable=true
 
 ovs-vsctl add-bond br-core bond-pcsc1-sc2 ens36 ens37 bond_mode=active-backup lacp=off other_config:bond-primary=ens36
-ovs-vsctl add-bond br-core bond-pcsc2-sd1 ens38 ens39 bond_mode=balance-slb lacp=off
-ovs-vsctl add-bond br-core bond-pcsc2-sd2 ens40 ens41 bond_mode=balance-slb lacp=off
+ovs-vsctl add-bond br-core bond-pcsc2-sd1 ens38 ens39 bond_mode=active-backup lacp=off other_config:bond-primary=ens38
+ovs-vsctl add-bond br-core bond-pcsc2-sd2 ens40 ens41 bond_mode=active-backup lacp=off other_config:bond-primary=ens40
 
-ovs-vsctl add-port br-core int-core-test -- set Interface int-core-test type=internal
-ovs-vsctl set port int-core-test tag=99
+# Configurar trunks para enlaces hacia distribución
+ovs-vsctl set port bond-pcsc2-sd1 trunks=10,20,30,40,50,60,70,80,99
+ovs-vsctl set port bond-pcsc2-sd2 trunks=10,20,30,40,50,60,70,80,99
 
 ip link set br-core up || true
 ip link set bond-pcsc1-sc2 up || true
 ip link set bond-pcsc2-sd1 up || true
 ip link set bond-pcsc2-sd2 up || true
-ip link set int-core-test up || true
 
-ip addr flush dev int-core-test || true
-ip addr add 10.255.21.2/30 dev int-core-test
+# Asignar IP al bridge br-core (enlace Core-Core)
+ip addr flush dev br-core || true
+ip addr add 10.255.21.2/30 dev br-core
 
 echo "[JHALEX] SWCORELIM2 - OVS configurado correctamente"
-echo "[JHALEX] Bridge: br-core | Bonds: sc2, sd1, sd2 | Internal: int-core-test 10.255.21.2/30"
+echo "[JHALEX] Bridge: br-core | Bonds: sc2, sd1, sd2 | IP: 10.255.21.2/30"
